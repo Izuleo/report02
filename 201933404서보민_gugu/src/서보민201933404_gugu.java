ae47a6578febcaf377a9108d317c4c7a53552ba5
special007 <special007@special007-PC>
2020-03-27 23:08:31 +0900

    201933404 서보민, 자료 구조 과제

---
+++ 201933404서보민_gugu/src/서보민201933404_gugu.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class 서보민201933404_gugu {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); //스캐너 생성
+		
+		System.out.println("원하는 하나의 수를 입력하세요: ");
+		int num = scanner.nextInt(); //입력받은 숫자를 num에 저장
+		
+		for(int i=0; i<10; i++) {
+			System.out.println(num + "X" + i + "=" + num*i);
+		}		
+		
+
+	}
+
+}
