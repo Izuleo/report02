4647d2f4b71092d160cd2b875128d2889d062c6b
special007 <special007@special007-PC>
2020-03-28 19:19:29 +0900

    자료구조 2번째 과제

---
+++ report02_1/src/report02_1/Student.java
@@ -0,0 +1,42 @@
+package report02_1;
+
+import java.util.Comparator;
+
+public class Student implements Comparable<Student> {
+   public static final Comparator<Student> WITH_NAME = new WithName();
+   public static final Comparator<Student> WITH_DEPT = new WithDept();
+   public static final Comparator<Student> WITH_GRADE = new WithGrade();
+   
+    int ID;
+    String name;
+    String dept;
+    Integer grade;
+   
+   public Student(int IDIn, String nameIn, String deptIn, int gradeIn) {
+      ID = IDIn;
+      name = nameIn;
+      dept = deptIn;
+      grade = gradeIn;
+   }
+   public int    getID()      {return ID;}
+   public String getName()     {return name;}
+   public String getDept()    {return dept;}
+   public int    getGrade()   {return grade;}
+    
+   public static class WithName implements Comparator<Student> { //이름 비교
+	   public int compare(Student s1, Student s2) {
+		   return s1.name.compareTo(s2.name);	   }
+   }
+   public static class WithDept implements Comparator<Student> { //학과 비교
+	   public int compare(Student s1, Student s2) {
+		   return s1.dept.compareTo(s2.dept);	   }
+   }
+   public static class WithGrade implements Comparator<Student> { //학년 비교
+	   public int compare(Student s1, Student s2) {
+		   return s1.grade-s2.grade;	   }
+   }
+   public int compareTo(Student s1) { //학번 비교
+	   return this.ID - s1.ID;
+   }
+   
+}
